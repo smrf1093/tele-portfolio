632691986cb025be34d31d8d020550876a9f9c7a
seyedrezafar <seyedrezafar1093@gmail.com>
2020-12-16 21:18:25 +0330

    Setup new ui, change flask config, merge themes

--- api.py
+++ api.py
@@ -1,10 +1,12 @@
+import requests
+
 def fetch_wallet_balance(address, currency='usd'):
     api_url = 'https://api.covalenthq.com'
     endpoint = f'/v1/1/address/{address}/balances_v2/'
     url = api_url + endpoint
     
     payload = {
-        "key": config("API_KEY"),
+        "key": 'ckey_d2b46519138441cdaca9c0f5fd0',# config("API_KEY"),
         "quote-currency": currency
     }
 
--- app.py
+++ app.py
@@ -1,17 +1,21 @@
 import os
-from flask import Flask, render_template, jsonify, request
+from flask import Flask, render_template, jsonify, request, send_from_directory
 from api import fetch_wallet_balance
 
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='')
 
+
+@app.route('/dist/<path:path>')
+def send_js(path):
+    return send_from_directory('dist', path)
 
 @app.route('/')
 def index():
     address = request.args.get('wallet')
     currency = request.args.get('currency')
     r = fetch_wallet_balance(address, currency)
-    return render_template('index.html',error=r, data=r['data'])
+    return render_template('layout-dark.html',error=r, data=r['data'])
 
 if __name__ == '__main__':
     app.run()
